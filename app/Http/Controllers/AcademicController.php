546bd4f07a2ef0960d42383cd7f5617901c54db8
Bipin Joshi <bipin.ingrails@gmail.com>
2025-07-19 22:15:27 +0545

    changes

--- app/Http/Controllers/AcademicController.php
+++ app/Http/Controllers/AcademicController.php
@@ -217,7 +217,16 @@ class AcademicController extends Controller
         // Get the currently authenticated user's ID
         $userId = auth()->id();
         $schoolId = session('school_id');
+        dd($schoolId);
         // dd(User::where('parent_id', $schoolId)->get());
+        if (!$userId) {
+            return response()->json(['error' => 'User ID is not authenticated.']);
+        }
+        if (!$schoolId) {
+            // Handle the case where school_id is not set or is null
+            // For example, you can throw an exception or return an error message
+            return response()->json(['error' => 'School ID is not set.']);
+        }
 
         // Delete related data for the authenticated user
         Staff::where('school_id', $schoolId)->delete();
